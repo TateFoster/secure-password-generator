c63fc1bdc67834b82d449721c8e607ee7b08b198
Dragon Lore <tate.j.foster@gmail.com>
2022-08-05 09:15:15 -0600

    got duel set perumations done as well as the ones with three sets working on small issue the ones with three sets

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 //Variable declartions
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var upppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var numerials = ["0", "1", "2", "3", "4", "5","6", "7", "8", "9"];
 
@@ -51,7 +51,7 @@ var generatePassword = function() {
             var uppercasePassword = function() {
               var uppercasePasswordReady = "";
               for (var i = 0; i < passwordLength; i++)
-               uppercasePasswordReady += upppercase[Math.floor(upppercase.length * Math.random())];
+               uppercasePasswordReady += uppercase[Math.floor(uppercase.length * Math.random())];
               return uppercasePasswordReady;
             }
             var passwordUpper = uppercasePassword();
@@ -78,23 +78,196 @@ var generatePassword = function() {
             return passwordSpecial;
 
           } else if (lowercaseInclude && uppercaseInclude && !numerialsInclude && !specialCharsInclude) {
-            var lowerUpper = lowercase.concat(upppercase);
+            var lowerUpper = lowercase.concat(uppercase);
 
             var lowerUpperPassword = function() {
               var lowerUpperPasswordReady = "";
               for (var i = 0; i < passwordLength; i++)
               lowerUpperPasswordReady += lowerUpper[Math.floor(lowerUpper.length * Math.random())];
               var lowerUpperTest = lowerUpperPasswordReady.split("")
-              console.log(lowerUpperTest)
-              console.log(lowerUpperTest.includes(lowercase))
-              console.log(lowerUpperTest.includes(upppercase))
-              if (lowerUpperTest)
+              
+              
+              if (lowerUpperTest.some(item => lowercase.includes(item)) && lowerUpperTest.some(item => uppercase.includes(item))) {
               return lowerUpperPasswordReady;
-            }
+              } else {
+                lowerUpperPassword();
+              }
+            } 
             var passwordLowerUpper = lowerUpperPassword();
             return passwordLowerUpper;
         
-          }
+          } else if (lowercaseInclude && !uppercaseInclude && numerialsInclude && !specialCharsInclude) {
+            var lowerNum = lowercase.concat(numerials);
+
+            var lowerNumPassword = function() {
+              var lowerNumPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              lowerNumPasswordReady += lowerNum[Math.floor(lowerNum.length * Math.random())];
+              var lowerNumTest = lowerNumPasswordReady.split("")
+              
+              
+              if (lowerNumTest.some(item => lowercase.includes(item)) && lowerNumTest.some(item => numerials.includes(item))) {
+              return lowerNumPasswordReady;
+              } else {
+                lowerNumPassword();
+              }
+            } 
+            var passwordLowerNum = lowerNumPassword();
+            return passwordLowerNum;
+        
+          } else if (lowercaseInclude && !uppercaseInclude && !numerialsInclude && specialCharsInclude) {
+            var lowerSpecial = lowercase.concat(specialChars);
+
+            var lowerSpecialPassword = function() {
+              var lowerSpecialPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              lowerSpecialPasswordReady += lowerSpecial[Math.floor(lowerSpecial.length * Math.random())];
+              var lowerSpecialTest = lowerSpecialPasswordReady.split("")
+              
+              
+              if (lowerSpecialTest.some(item => lowercase.includes(item)) && lowerSpecialTest.some(item => specialChars.includes(item))) {
+              return lowerSpecialPasswordReady;
+              } else {
+                lowerSpecialPassword();
+              }
+            } 
+            var passwordLowerSpecial = lowerSpecialPassword();
+            return passwordLowerSpecial;
+        
+          } else if (!lowercaseInclude && uppercaseInclude && numerialsInclude && !specialCharsInclude) {
+            var upperNum = uppercase.concat(numerials);
+
+            var upperNumPassword = function() {
+              var upperNumPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              upperNumPasswordReady += upperNum[Math.floor(upperNum.length * Math.random())];
+              var upperNumTest = upperNumPasswordReady.split("")
+              
+              
+              if (upperNumTest.some(item => uppercase.includes(item)) && upperNumTest.some(item => numerials.includes(item))) {
+              return upperNumPasswordReady;
+              } else {
+                upperNumPassword();
+              }
+            } 
+            var passwordUpperNum = upperNumPassword();
+            return passwordUpperNum;
+        
+          } else if (!lowercaseInclude && uppercaseInclude && !numerialsInclude && specialCharsInclude) {
+            var upperSpecial = uppercase.concat(specialChars);
+
+            var upperSpecialPassword = function() {
+              var upperSpecialPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              upperSpecialPasswordReady += upperSpecial[Math.floor(upperSpecial.length * Math.random())];
+              var upperSpecialTest = upperSpecialPasswordReady.split("")
+              
+              
+              if (upperSpecialTest.some(item => uppercase.includes(item)) && upperSpecialTest.some(item => specialChars.includes(item))) {
+              return upperSpecialPasswordReady;
+              } else {
+                upperSpecialPassword();
+              }
+            } 
+            var passwordUpperSpecial = upperSpecialPassword();
+            return passwordUpperSpecial;
+        
+          } else if (!lowercaseInclude && !uppercaseInclude && numerialsInclude && specialCharsInclude) {
+            var numSpecial = numerials.concat(specialChars);
+
+            var numSpecialPassword = function() {
+              var numSpecialPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              numSpecialPasswordReady += numSpecial[Math.floor(numSpecial.length * Math.random())];
+              var numSpecialTest = numSpecialPasswordReady.split("")
+              
+              
+              if (numSpecialTest.some(item => numerials.includes(item)) && numSpecialTest.some(item => specialChars.includes(item))) {
+              return numSpecialPasswordReady;
+              } else {
+                numSpecialPassword();
+              }
+            } 
+            var passwordNumSpecial = numSpecialPassword();
+            return passwordNumSpecial;
+        
+          } else if (lowercaseInclude && uppercaseInclude && numerialsInclude && !specialCharsInclude) {
+            var lowerUpperNum = lowercase.concat(uppercase.concat(numerials));
+
+            var lowerUpperNumPassword = function() {
+              var lowerUpperNumPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              lowerUpperNumPasswordReady += lowerUpperNum[Math.floor(lowerUpperNum.length * Math.random())];
+              var lowerUpperNumTest = lowerUpperNumPasswordReady.split("")
+              
+              
+              if (lowerUpperNumTest.some(item => lowercase.includes(item)) && lowerUpperNumTest.some(item => uppercase.includes(item)) && lowerUpperNumTest.some(item => numerials.includes(item))) {
+              return lowerUpperNumPasswordReady;
+              } else {
+                lowerUpperNumPassword();
+              }
+            } 
+            var passwordLowerUpperNum = lowerUpperNumPassword();
+            return passwordLowerUpperNum;
+        
+          } else if (lowercaseInclude && uppercaseInclude && !numerialsInclude && specialCharsInclude) {
+            var lowerUpperSpecial = lowercase.concat(uppercase.concat(specialChars));
+
+            var lowerUpperSpecialPassword = function() {
+              var lowerUpperSpecialPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              lowerUpperSpecialPasswordReady += lowerUpperSpecial[Math.floor(lowerUpperSpecial.length * Math.random())];
+              var lowerUpperSpecialTest = lowerUpperSpecialPasswordReady.split("")
+              
+              
+              if (lowerUpperSpecialTest.some(item => lowercase.includes(item)) && lowerUpperSpecialTest.some(item => uppercase.includes(item)) && lowerUpperSpecialTest.some(item => specialChars.includes(item))) {
+              return lowerUpperSpecialPasswordReady;
+              } else {
+                lowerUpperSpecialPassword();
+              }
+            } 
+            var passwordLowerUpperSpecial = lowerUpperSpecialPassword();
+            return passwordLowerUpperSpecial;
+        
+          } else if (lowercaseInclude && !uppercaseInclude && numerialsInclude && specialCharsInclude) {
+            var lowerNumSpecial = lowercase.concat(numerials.concat(specialChars));
+
+            var lowerNumSpecialPassword = function() {
+              var lowerNumSpecialPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              lowerNumSpecialPasswordReady += lowerNumSpecial[Math.floor(lowerNumSpecial.length * Math.random())];
+              var lowerNumSpecialTest = lowerNumSpecialPasswordReady.split("")
+              
+              
+              if (lowerNumSpecialTest.some(item => lowercase.includes(item)) && lowerNumSpecialTest.some(item => numerials.includes(item)) && lowerNumSpecialTest.some(item => specialChars.includes(item))) {
+              return lowerNumSpecialPasswordReady;
+              } else {
+                lowerNumSpecialPassword();
+              }
+            } 
+            var passwordLowerNumSpecial = lowerNumSpecialPassword();
+            return passwordLowerNumSpecial;
+        
+          } else if (!lowercaseInclude && uppercaseInclude && numerialsInclude && specialCharsInclude) {
+            var upperNumSpecial = uppercase.concat(numerials.concat(specialChars));
+
+            var upperNumSpecialPassword = function() {
+              var upperNumSpecialPasswordReady = "";
+              for (var i = 0; i < passwordLength; i++)
+              upperNumSpecialPasswordReady += upperNumSpecial[Math.floor(upperNumSpecial.length * Math.random())];
+              var upperNumSpecialTest = upperNumSpecialPasswordReady.split("")
+              console.log(upperNumSpecialPasswordReady)
+              
+              if (upperNumSpecialTest.some(item => uppercase.includes(item)) && upperNumSpecialTest.some(item => numerials.includes(item)) && upperNumSpecialTest.some(item => specialChars.includes(item))) {
+              return upperNumSpecialPasswordReady;
+              } else {
+                upperNumSpecialPassword();
+              }
+            } 
+            var passwordUpperNumSpecial = upperNumSpecialPassword();
+            return passwordUpperNumSpecial;
+        
+          } 
         
         }
         var passwordCombine = combineChars();
